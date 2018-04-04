bec66a05dcccc8fe8da7ca731f3abfc01c2c8308
paalk93 <paal_93@hotmail.com>
2018-04-04 14:07:53 +0200

    added javascript file to webshop.

---
+++ JavaScript/javascript.js
@@ -0,0 +1,11 @@
+
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+  if(username == lolhei123 && password == lol123){
+    alert("Login successful")
+  }
+  else{
+    alert("Wrong login")
+  }
+}
