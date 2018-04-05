4b2d61925171db3421f81b4fafb8d08c4b53b6e3
paalk93 <paal_93@hotmail.com>
2018-04-05 09:31:24 +0200

    added redirect on login button on successful login.

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -2,10 +2,11 @@
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-  if(username == lolhei123 && password == lol123){
-    alert("Login successful")
-  }
-  else{
-    alert("Wrong login")
-  }
+if ( username == "hei123" && password == "lol456"){
+alert ("Welcome " + username);
+  window.location ="user.html";
+}
+ else{
+   alert("Wrong login")
+ }
 }
