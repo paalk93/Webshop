5e9d2e0a9cc3e67bff2aa2d80d2264f680a68e98
paalk93 <paal_93@hotmail.com>
2018-04-05 14:45:02 +0200

    Played around with login and user page using javascript.

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -3,10 +3,10 @@ function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "hei123" && password == "lol456"){
-alert ("Welcome " + username);
+  alert ("Welcome " + username);
   window.location ="user.html";
 }
- else{
+else {
    alert("Wrong login")
  }
 }
